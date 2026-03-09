cc722bd82b6ce109ae2857bda86ba9f810479067
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-09 20:16:10 +0700

    input

--- BMI.cpp
+++ BMI.cpp
@@ -1,3 +1,12 @@
 //library
 #include <iostream>
 using namespace std;
+
+//input
+void input(float &berat, float &tinggi) {
+    cout << "Masukkan Berat badn (kg): ";
+    cin >> berat;
+
+    cout << "Masukkan Tinggi badan (meter): ";
+    cin >> tinggi;
+}
